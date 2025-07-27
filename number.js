5ac10b22c9a78ba009f68ef53e5b4f964821c601
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-07-27 11:09:12 +0600

    some aditional information include the numbering system in js

---
+++ number.js
@@ -0,0 +1,26 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+// this is a string i know that
+// convert the sting that name is parseInt
+// NAN means not a number
+var applePrice = parseInt('26');
+var all_pirce = parseFloat('22.5');
+
+// console.log(typeof orangePrice);
+// console.log(typeof applePrice);
+console.log(orangePrice + applePrice);
+
+var first = 0.1;
+var  se= 0.2;
+var total = first+ se;
+// this is to count after doshomik remember this
+console.log(total.toFixed(5));
+// there is more to explore
+
+// this is infinity but positive infinity
+const num = 50/0;
+console.log(num);
+
+// negative infinity
+const n = -50/0;
+console.log(n);
