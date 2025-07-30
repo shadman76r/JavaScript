c6b221a46b4c0b12e387ad733658b1529005f645
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-07-30 10:34:59 +0600

    while

--- loops/while.js
+++ loops/while.js
@@ -3,3 +3,21 @@ if (number <10){
     console.log('i found a little number')
 }
 console.log('something new is cooking')
+
+
+// infinete while loop
+// let num =0; //loops;
+// while(num<5){
+//     console.log('looping', num);
+//     // num = num+1;
+// }
+// simple loop
+
+let num =0; //loops;
+while(num<5){
+    console.log('looping', num);
+    // num = num+1
+    // num = num+1;
+    // num ++;
+    num += 1;
+}
