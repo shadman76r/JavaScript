e557c5e5817703f74dcf70194968a53330f2b4fc
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-07-30 17:23:27 +0600

    task

---
+++ loops/range.js
@@ -0,0 +1,25 @@
+// print 1 to 10 number using while loop
+let num = 1;
+while (num<=10){
+    console.log(num);
+    num++;
+}
+
+// 
+
+let num1 = 50;
+while(num1<=100){
+    console.log(num1);
+    num1++;
+}
+//
+let number = 1;
+let sum = 0;
+while(number<=10){
+    console.log(number);
+    sum = sum+number;
+    console.log('sum:' , sum);
+    number ++;
+}
+
+//even
