080eecf7384156a9a6b16614c4e360e00fdb57c3
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-07-30 10:15:41 +0600

    loops

---
+++ loops/recap.js
@@ -0,0 +1,16 @@
+//const: value will not change
+// let: value can change
+// var : we should not use it
+// 
+const amount = 200;
+if(amount>100){
+    console.log("yoo");
+}
+else{
+    console.log("no");
+}
+
+// array
+
+const numbers = [12, 32, 34,35];
+console.log(numbers)
