867b3e0d339da113afaf6ff389efde435118b3e6
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-07-30 10:27:05 +0600

    while

---
+++ loops/for.js
@@ -0,0 +1,16 @@
+// fixed the array
+const number = [12, 98, 45, 63, 21, 72]
+console.log(number);
+
+// 
+
+for(const a of number){
+    console.log(a);
+}
+
+const f=['orange0', 'apple', 'banana', 'jackfruits','other']
+
+for(const fs of f){
+    console.log(fs);
+    console.log('i want to eat');
+}
---
+++ loops/while.js
@@ -0,0 +1,5 @@
+const number = 7;
+if (number <10){
+    console.log('i found a little number')
+}
+console.log('something new is cooking')
