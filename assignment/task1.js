91943e23258d30d553e40954751d8a9fd15adfc4
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-03 20:57:16 +0600

    task-1

---
+++ assignment/task1.js
@@ -0,0 +1,4 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 2060;
+//write your code here
+console.log(area/2);
--- loops/for.js
+++ loops/for.js
@@ -1,16 +1,30 @@
-// fixed the array
-const number = [12, 98, 45, 63, 21, 72]
-console.log(number);
+// // fixed the array
+// const number = [12, 98, 45, 63, 21, 72]
+// console.log(number);
 
-// 
+// // 
 
-for(const a of number){
-    console.log(a);
+// for(const a of number){
+//     console.log(a);
+// }
+
+// const f=['orange0', 'apple', 'banana', 'jackfruits','other']
+
+// for(const fs of f){
+//     console.log(fs);
+//     console.log('i want to eat');
+// }
+
+//simple for loop
+
+for(let numbers =0; numbers<5;numbers++){
+    console.log(numbers);
 }
 
-const f=['orange0', 'apple', 'banana', 'jackfruits','other']
+for(let i =1 ; i<=20;i++){
+    console.log(i);
+}
 
-for(const fs of f){
-    console.log(fs);
-    console.log('i want to eat');
+for(let i=0;i<=20;i=i+2){
+    console.log(i)
 }
