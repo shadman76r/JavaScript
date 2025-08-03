95dafa1da08b67c391b74c46d03a9f09d772848f
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-03 22:13:23 +0600

    task6 done

---
+++ assignment/task6.js
@@ -0,0 +1,5 @@
+/** Problem 06 :  (Current Salary )  */
+var experience = 40;
+var startingSalary = 30000;
+//write your code here
+console.log((startingSalary * (1+0.05) ** experience).toFixed(2));
