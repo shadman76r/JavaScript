437846ddf91951f614d5da2154f8b03e0b53f5c1
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-03 21:05:20 +0600

    task-2

---
+++ assignment/task2.js
@@ -0,0 +1,13 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 9999;
+//write your code here
+
+if(money >=25000){
+    console.log("Laptop");
+}
+else if(money >=10000 && money<25000){
+    console.log("Cycle");
+}
+else{
+    console.log("Chocolate");
+}
