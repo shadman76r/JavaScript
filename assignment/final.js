345408283c84a9f459ae5abebed6e91fb2ce5f5d
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-03 22:14:02 +0600

    all done in final file

--- assignment/final.js
+++ assignment/final.js
@@ -43,3 +43,9 @@ else{
 var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
 //write your code here
 console.log(student.name + student.roll + "." +student.department + "@ph.ac.bd");
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 40;
+var startingSalary = 30000;
+//write your code here
+console.log((startingSalary * (1+0.05) ** experience).toFixed(2));
