eb55fd1f9f725f4943fea937becaa9c6d7b915df
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-03 21:14:02 +0600

    task3

--- assignment/task3.js
+++ assignment/task3.js
@@ -1,3 +1,11 @@
 /** Problem -03 ( Medicine Planner ) */
-var lastDay = 11 ;
+var lastDay = 4 ;
 //write your code here
+for( var i=1; i<= lastDay; i++){
+    if(i%3===0){
+        console.log(i + " - medicine");
+    }
+    else{
+        console.log(i + " - rest");
+    }
+}
