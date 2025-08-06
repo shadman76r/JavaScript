62d0158dd0e7e14bda9076ccea0d607cacac9ba5
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-06 17:34:22 +0600

    sort half

---
+++ array/sort.js
@@ -0,0 +1,8 @@
+const friends=['sakib','akib','rakib','forkruddin'];
+friends.sort();
+console.log(friends);
+
+const ages = [1,5,100,15,8,2,28];
+console.log(ages);
+ages.sort();
+console.log(ages);
