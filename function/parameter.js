2c4cfe018c7f537cb4966e5e366968f08b7e2620
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-07 23:19:44 +0600

    parameters

---
+++ function/parameter.js
@@ -0,0 +1,28 @@
+function eat (){
+    console.log("you can eat anywhere")
+    console.log("take food in your plates")
+}
+eat();
+
+// square
+
+function square(number){
+    console.log(number);
+    console.log('value of the number parameter: ', number);
+    const borgo = number * number;
+    console.log('square of the given number is: ', borgo)
+}
+
+square(4);
+console.log('..............')
+square(13);
+
+
+// 
+function add(number1, number2){
+    const sum= number1+number2;
+    console.log(sum);
+
+}
+add(5,71);
+add(65,31);
