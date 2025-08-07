f7578144f9a17e110210c334e1dac43842644f1d
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-08 00:30:10 +0600

    return

---
+++ function/return.js
@@ -0,0 +1,17 @@
+function time(number){
+    const result = number *10
+    return result;
+
+}
+const output = time(5);
+console.log(output);
+
+// another one
+function cuthalf(number){
+const half = number/2;
+return half;
+}
+time(6)
+// need to so the for return must be do this or i can not use the return tha function
+// const output = time(5);
+// console.log(output);
