29dd89fa505cddeb72e7e0fbcb0502e564259efb
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-07 20:49:29 +0600

    sort new items

--- array/sort.js
+++ array/sort.js
@@ -6,3 +6,18 @@ const ages = [1,5,100,15,8,2,28];
 console.log(ages);
 ages.sort();
 console.log(ages);
+
+const numbers = [6,5,8,7,9,1];
+console.log(numbers);
+numbers.sort();
+console.log(numbers);
+
+//full new thing 
+//new thing need to know how it work
+//asending
+const sort_ages= ages.sort(function(a,b){return a-b})
+console.log(sort_ages);
+
+//desending
+const sort_ages_dec = ages.sort(function(a,b){return b-a})
+console.log(sort_ages_dec);
