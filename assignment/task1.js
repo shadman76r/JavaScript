183b2362c2c5f9d50b031f1f5c6ac4c2005a4397
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-19 06:17:23 +0600

    dom manupulation

--- assignment/task1.js
+++ assignment/task1.js
@@ -1,7 +1,7 @@
-// /** Problem -01 ( Divide the Asset ) */
-// var area = 2060;
-// //write your code here
-// console.log(area/2);
+/** Problem -01 ( Divide the Asset ) */
+var area = 2060;
+//write your code here
+console.log(area/2);
 
 // var A = 10;
 // console.log(A);
@@ -10,10 +10,10 @@
 // var $var = 10;
 // var a_b_c 
 
-var a = 10;
-var a = 59;
+// var a = 10;
+// var a = 59;
 
-function a(){
-    if
-}
-a();
+// function a(){
+//     if
+// }
+// a();
