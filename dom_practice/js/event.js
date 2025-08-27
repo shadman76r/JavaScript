3301ca7195c8526a879552f8fd9960ede8678605
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-28 00:34:17 +0600

    event listner

--- dom_practice/js/event.js
+++ dom_practice/js/event.js
@@ -1,28 +1,28 @@
 console.log('external file');
 // option 2 to handel event
- function make(){
-            document.body.style.backgroundColor = 'yellow'
-        }
-        function red(){
-            document.body.style.backgroundColor = 'red'
-        }
+function make() {
+    document.body.style.backgroundColor = 'yellow'
+}
+function red() {
+    document.body.style.backgroundColor = 'red'
+}
 
 // option 3
-  // chamel case
-        const btnMakeBlue = document.getElementById('btn-make-blue')
-        // console.log(btnMakeBlue);
-        btnMakeBlue.onclick = function makeblue(){
-            document.body.style.backgroundColor = 'blue'
-        }
+// chamel case
+const btnMakeBlue = document.getElementById('btn-make-blue')
+// console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeblue() {
+    document.body.style.backgroundColor = 'blue'
+}
 
 // option 3 different kind
 
-        const btnMakePurple = document.getElementById('btn-make-purple');
+const btnMakePurple = document.getElementById('btn-make-purple');
 
-    //   i can write just function name if i want to use it
+//   i can write just function name if i want to use it
 
-      btnMakePurple.onclick = purple;
+btnMakePurple.onclick = purple;
 
-      function purple(){
-        document.body.style.backgroundColor = 'purple'
-      }
+function purple() {
+    document.body.style.backgroundColor = 'purple'
+}
