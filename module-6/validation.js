f82c1ca34b244ec620a129dabcda8cf82236d4a0
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-09-06 14:04:50 +0600

    module-6 start

---
+++ module-6/validation.js
@@ -0,0 +1,20 @@
+const number = '23';
+if(typeof number === 'number'){
+    console.log('value is a number')
+}
+else{
+    console.log('value is not a number')
+}
+
+const numbers = [12,14,78,999]
+const studen = { name: 'name', job : 'always name'}
+console.log(Array.isArray(numbers))
+console.log(typeof studen)
+
+
+// this can be carry by const
+const dress = [];
+dress.push('name')
+
+// different between var let and const in javascript
+
